72466ef0fc66c53f27ff0dfae17199e45d923451
evelinaoleinic <evelina.oleinic1@gmail.com>
2024-03-05 15:18:43 +0200

    ex

---
+++ tema.js
@@ -0,0 +1,19 @@
+// let firstName = 'eva'
+// let lastName = 'oleinic'
+// let streetAddress = 'Ioan Botezatorul'
+
+// let NUME_DE_FAMILIE = 'Topala'
+// let DATA_DE_LOGARE = 29.03
+// let PRETUL_ACHITAT = 100
+
+// let Nume = 'Oleinic'
+// let Prenume = 'Eva'
+// let Locuinta = 'Chisinau'
+
+
+
+const nume = 'eva'
+const prenume = 'oleinic'
+const varsta = 24
+console.log(`informatie ${nume} ${prenume} ${varsta}`)
+
